53cd74f041208e4841c626a0c79350c643457a48
Samuel Oluwatobi Ayoola <ayoolasam2019@gmail.com>
2025-07-18 06:54:25 +0100

    Fix MongoDB URI double-appending

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 
-const connectDB =async ()=>{
+const connectDB = async () => {
     try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/TheIncubator`)
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+        await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
         console.log(error.message);
     }
 }
 
-export default connectDB
+export default connectDB;
